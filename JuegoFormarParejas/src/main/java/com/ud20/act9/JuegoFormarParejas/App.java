5e650ff01a1ff4f864606603f7d0f3987e6daaf2
adriaqf <adriaqf@gmail.com>
2022-05-03 11:39:24 +0200

    primer commit

---
+++ JuegoFormarParejas/src/main/java/com/ud20/act9/JuegoFormarParejas/App.java
@@ -0,0 +1,13 @@
+package com.ud20.act9.JuegoFormarParejas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
